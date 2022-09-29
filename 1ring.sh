a32998d63b9bfd887ddd199f06842d7ad91b8c5a
chaslinux <chaslinux@gmail.com>
2022-09-29 12:40:42 -0400

    Added some colour-coding to make the script more appealing.

--- 1ring.sh
+++ 1ring.sh
@@ -1,14 +1,26 @@
-#/bin/bash
+#!/bin/bash
+# Copyright 2022 Chaslinux - chaslinux@gmail.com
+# Licensed under GPL v3.0
 
+WHITE='\033[1;37m'
+NC='\033[0m'
+LTGREEN='\033[1;32m'
+PURPLE="\033[0;35m'
+
+echo -e "${LTGREEN}*** ${WHITE}Running updates & installing git! ${LTGREEN}*** ${NC}"
 sudo apt update && sudo apt -y upgrade
 sudo apt install git -y
 
+echo -e "${LTGREEN}*** ${WHITE}Making the Code directory & cloning the repositories ${LTGREEN}*** ${NC}"
 mkdir ~/Code
 cd ~/Code
 
 git clone https://github.com/chaslinux/hardware.sh
 git clone https://github.com/chaslinux/install-extras.sh
 git clone https://github.com/chaslinux/phoronix/
+
+echo -e "${LTGREEN}*** ${WHITE}Start to run the scripts! ${LTGREEN}*** ${NC}"
+echo -e "${PURPLE}*** ${WHITE}Note: This part is NOT completely automated! ${PURPLE}*** ${NC}"
 
 cd ~/Code/hardware.sh
 ./hardware.sh
